71e11cc437ea6eaa2bb34daaf4100379f58bfdf8
MrDomoArigato98 <domdob98@gmail.com>
2025-06-23 12:25:25 +0100

    Scaling navbar links on hover

--- cart/src/components/Navbar.jsx
+++ cart/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
-import cart from "../assets/cart.svg";
 import { FaShoppingCart } from "react-icons/fa";
 export default function Navbar() {
   return (
     <nav>
-      <span>Dom-store.com</span>
+      <NavLink to="/">
+        <h1 className="scalable">Dom-store.com</h1>
+      </NavLink>
+
       <div className="nav-middle">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/shop">Shop</NavLink>
+        <NavLink className="scalable" style={{fontSize:"1.5rem"}} to="/">Home</NavLink>
+        <NavLink className="scalable" style={{fontSize:"1.5rem"}} to="/shop">Shop</NavLink>
       </div>
 
-      <NavLink to="/cart" className="nav-link-icon">
+      <NavLink  to="/cart" className="nav-link-icon scalable">
         <FaShoppingCart className="icon" />
         <span>Cart</span>
       </NavLink>
