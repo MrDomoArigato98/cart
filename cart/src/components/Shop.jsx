aa6cfeac8fa2b08a93d02ca8d7c820e433f1b391
MrDomoArigato98 <domdob98@gmail.com>
2025-06-26 10:47:06 +0100

    context api

--- cart/src/components/Shop.jsx
+++ cart/src/components/Shop.jsx
@@ -1,9 +1,10 @@
 export default function Shop(){
 
+
+    // https://fakestoreapi.com/products/category/electronics
     return (
         <>
         <h1>I am the shop</h1>
-        
         </>
     )
 }
