55839b5f0bb6e8b0deaac947b1d16f1930d58551
MrDomoArigato98 <domdob98@gmail.com>
2025-06-26 09:52:34 +0100

    Adding context api for props

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -1,16 +1,30 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { useState, createContext } from "react";
+
+export const ShopContext = createContext({
+  products: [],
+  cartItems: [],
+  addToCart: () => {},
+});
 
 export default function App() {
+  const [cartItems, setCartItems] = useState(["Shirt", "Trousers"]);
+  const products =
+    []; /* some custom hook that fetches products and returns the fetched products */
 
-
+  const addToCart = () => {
+    // add to cart logic (this adds to cartItems)
+  };
   return (
     <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
+      <ShopContext.Provider value={{ cartItems, products, addToCart }}>
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+      </ShopContext.Provider>
       <Footer />
     </>
   );
--- cart/src/components/Navbar.jsx
+++ cart/src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaShopify } from "react-icons/fa6";
+import { useContext } from "react";
+import { ShopContext } from "../App";
+
+
 export default function Navbar() {
+  const { cartItems } = useContext(ShopContext);
   return (
     <nav>
       <NavLink to="/">
@@ -25,7 +30,7 @@ export default function Navbar() {
         <FaShoppingCart className="icon-small" />
         <span>Cart</span>
         {/* Here I'll need to have the total number of items in the cart */}
-        <span>0</span>
+        <span>{cartItems.length}</span>
       </NavLink>
     </nav>
   );
