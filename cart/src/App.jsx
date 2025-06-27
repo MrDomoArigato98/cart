2995b5dc6f83c4c94fe5ff0671852638fec3825f
MrDomoArigato98 <domdob98@gmail.com>
2025-06-27 12:08:03 +0100

    fetches products

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 
 export const ShopContext = createContext({
   products: [],
@@ -11,13 +11,26 @@ export const ShopContext = createContext({
 
 export default function App() {
   const [cartItems, setCartItems] = useState(["Shirt", "Trousers"]);
-  const products =
-    []; /* some custom hook that fetches products and returns the fetched products */
+  const [products, setProducts] = useState([]);
 
   const addToCart = () => {
     // add to cart logic (this adds to cartItems)
   };
-  return (
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error("Server Error");
+        }
+        return res.json();
+      })
+      .then((res) => setProducts(res));
+  }, []);
+    
+    console.log(products);
+  
+    return (
     <>
       <ShopContext.Provider value={{ cartItems, products, addToCart }}>
         <Navbar />
--- cart/src/components/Shop.jsx
+++ cart/src/components/Shop.jsx
@@ -1,10 +1,20 @@
-export default function Shop(){
+import { useEffect } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../App";
+import "../styles/shop.css"
 
+export default function Shop() {
+  const { products, addToCart } = useContext(ShopContext);
 
-    // https://fakestoreapi.com/products/category/electronics
-    return (
-        <>
-        <h1>I am the shop</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>I am the shop</h1>
+
+      <div className="products">
+        {products.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+      </div>
+    </>
+  );
 }
